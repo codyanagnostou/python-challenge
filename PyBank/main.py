eb9652cb4e80fcfd36bf385ea83f5a76a9e72746
Cody Anagnostou <codyanagnostou@icloud.com>
2023-01-31 00:01:15 -0800

    getting somewhere

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,7 +4,10 @@ import os
 import csv
 
 # Create file path
-csvpath = os.path.join("Resources", "budget_data.csv")
+budget_csv = os.path.join("Resources", "budget_data.csv")
+
+#Create function
+
 
 # Lists to store data
 total_months = []
@@ -12,13 +15,19 @@ total = []
 avg_change = []
 price_increase = []
 price_decrease = []
-grad_rate = []
+
+# with open(budget_csv, encoding='utf-8') as csvfile:
+with open(budget_csv, encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader, None)
+    for row in csvreader:
+        #Add up total months
+        total_months.append(row[0])
 
 # Print 
 print(f"----------------------------")
-print(f"Total Months: ")
+print(f"Total Months: {len(total_months)}")
 print(f"Total: ")
 print(f"Average Change: ")
 print(f"Greatest Increase in Profits: ")
-print(f"Overall Graduation Rate: ")
 print(f"Greatest Decrease in Profits: ")
