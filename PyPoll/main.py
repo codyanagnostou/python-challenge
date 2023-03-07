a2e2b0e1630314c893010ed0c2df5863550f3bd1
Cody Anagnostou <codyanagnostou@icloud.com>
2023-03-07 14:46:20 -0800

    working on pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,22 @@
 # Import CSV modules 
-
 import os
 import csv
+
+# Create file path
+election_csv = os.path.join("Resources", "election_data.csv")
+
+#Lists to store data 
+total_votes = []
+
+# with open(budget_csv, encoding='utf-8') as csvfile:
+with open(election_csv, encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader, None)
+    for row in csvreader:
+        #Add up total months
+        total_votes.append(row[0])
+
+# Print results
+print(f"Election Results")
+print(f"----------------------------")
+print(f"Total Votes: {len(total_votes)}")
